29a65e672b4cdcb4b5f1c88c3a08da2ab5c5d732
Giorgio Miglia <48253331+GiorgioMiglia@users.noreply.github.com>
2021-09-15 11:39:14 +0200

    Update theButtonBot.ino
    some minor changes in the comments

--- theButtonBot.ino
+++ theButtonBot.ino
@@ -29,15 +29,15 @@ char ssid[] = "XXXXXXX";     // your network SSID (name)
 char password[] = "XXXXXXXX"; // your network key
 
 // Initialize Telegram BOT
-#define BOTtoken "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" // your Bot Token (Get from Botfather)
+#define BOTtoken "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" // your Bot Token (Get from @Botfather)
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
 String photo_url = "https://vignette.wikia.nocookie.net/among-us-wiki/images/1/18/Emergency_Meeting.png/revision/latest?cb=20200803160022"; //the url of the pic
-#define buttonPin D1 //change D1 with your pin (must have pullup)
+#define buttonPin D1 //change D1 with your pin (must have pullup or it will need a 10k ohm resistor)
 int old_status = 1;
-String chat_id = "XXXXXXX"; // your or your group chat id (use myidbot)
+String chat_id = "XXXXXXX"; // your or your group chat id (use @myidbot)
 
 void setup() {
   client.setInsecure();
@@ -55,7 +55,7 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_BUILTIN, old_status);
-  if (digitalRead(buttonPin) == 0 && digitalRead(buttonPin) != old_status) { //prevent spamming
+  if (digitalRead(buttonPin) == 0 && digitalRead(buttonPin) != old_status) { //prevent spamming (debouncing the button)
     old_status = 0;
     digitalWrite(LED_BUILTIN, old_status); // used for feedback
     bot.sendPhoto(chat_id, photo_url, ""); // you can write what you want as pic description between ""
